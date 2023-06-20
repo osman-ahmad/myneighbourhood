83cd5f59005d3f17e56c68e8608d2444cf27cc46
osman-ahmad <satriamillenium@yahoo.com.sg>
2023-06-21 00:54:12 +0800

    first commit

---
+++ server/src/main/java/ibf2022/miniproject/myneighbourhood/MyneighbourhoodApplication.java
@@ -0,0 +1,13 @@
+package ibf2022.miniproject.myneighbourhood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyneighbourhoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyneighbourhoodApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/ibf2022/miniproject/myneighbourhood/MyneighbourhoodApplicationTests.java
@@ -0,0 +1,13 @@
+package ibf2022.miniproject.myneighbourhood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyneighbourhoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
